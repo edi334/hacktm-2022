56d7ae71a63a70174952e6987ff87b6308a84b75
Adrian Solomon <adrian.solomon@student.upt.ro>
2022-06-10 23:33:28 +0300

    fixed shared + added definitely not top bar component

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefinitelyNotHomePageComponent } from './definitely-not-pages/definitely-not-home-page/definitely-not-home-page.component';
+import { DefinitelyNotTopBarComponent } from './shared/definitely-not-top-bar/definitely-not-top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefinitelyNotHomePageComponent
+    DefinitelyNotHomePageComponent,
+    DefinitelyNotTopBarComponent
   ],
   imports: [
     BrowserModule,
