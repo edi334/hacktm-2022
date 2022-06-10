9d1cbc421501691557c9bdcc26a88f2e9e53dd91
Adrian Solomon <adrian.solomon@student.upt.ro>
2022-06-10 23:23:33 +0300

    fixed routing

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DefinitelyNotHomePageComponent} from "./definitely-not-pages/definitely-not-home-page/definitely-not-home-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefinitelyNotHomePageComponent,
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    component: DefinitelyNotHomePageComponent,
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
