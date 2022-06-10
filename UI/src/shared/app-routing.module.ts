3f682e53838b1ee27fe1ce3bdd29b62e7169273f
Adrian Solomon <adrian.solomon@student.upt.ro>
2022-06-10 23:19:21 +0300

    Added router

---
+++ UI/src/shared/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {DefinitelyNotHomePageComponent} from "../app/definitely-not-pages/definitely-not-home-page/definitely-not-home-page.component";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefinitelyNotHomePageComponent,
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    component: DefinitelyNotHomePageComponent,
+    pathMatch: 'full'
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
