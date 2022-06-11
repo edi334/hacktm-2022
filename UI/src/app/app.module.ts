3746e3a8836397024d926134f090cb32924c9e94
Adrian Solomon <adrian.solomon@student.upt.ro>
2022-06-11 23:30:29 +0300

    Git tampit

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -10,7 +10,6 @@ import {DefinitelyNotTopBarComponent} from './shared/definitely-not-top-bar/defi
 import {
   DefinitelyNotRegisterComponent
 } from './definitely-not-pages/definitely-not-register/definitely-not-register.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
@@ -20,38 +19,28 @@ import {
 } from './definitely-not-pages/definitely-not-login-page/definitely-not-login-page.component';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
-<<<<<<< HEAD
 import { DefinitelyNotQuizPageComponent } from './definitely-not-pages/definitely-not-quiz-page/definitely-not-quiz-page.component';
 import {MatRadioModule} from "@angular/material/radio";
 import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-=======
 import {HttpClientModule} from "@angular/common/http";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import { DefinitelyNotAButtonComponent } from './shared/definitely-not-a-button/definitely-not-a-button.component';
->>>>>>> main
 
 @NgModule({
   declarations: [
     AppComponent,
     DefinitelyNotHomePageComponent,
     DefinitelyNotTopBarComponent,
-<<<<<<< HEAD
-    DefinitelyNotQuizPageComponent
-=======
+    DefinitelyNotQuizPageComponent,
     DefinitelyNotRegisterComponent,
     DefinitelyNotLoginPageComponent,
     DefinitelyNotAButtonComponent
->>>>>>> main
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     MatToolbarModule,
     MatRadioModule,
     MatFormFieldModule,
@@ -60,8 +49,7 @@ import { DefinitelyNotAButtonComponent } from './shared/definitely-not-a-button/
     MatButtonModule,
     MatIconModule,
     FormsModule,
-    FontAwesomeModule
-=======
+    FontAwesomeModule,
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
@@ -72,7 +60,6 @@ import { DefinitelyNotAButtonComponent } from './shared/definitely-not-a-button/
     MatToolbarModule,
     HttpClientModule,
     MatSnackBarModule
->>>>>>> main
   ],
   providers: [],
   bootstrap: [AppComponent]
